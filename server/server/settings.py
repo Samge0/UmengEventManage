e94a49606a73c661ff8580eead3f4a9b8cd5e1e6
Samge <948331124@qq.com>
2021-12-22 18:34:22 +0800

    设置django所有ip可访问

--- server/server/settings.py
+++ server/server/settings.py
@@ -26,8 +26,11 @@ SECRET_KEY = 'django-insecure-!m-+)@%lmta**b2%e%w*eh9z#u$-lxd$y#q^b3#u-5vvc^uqma
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '127.0.0.1'
+    '*'
 ]
+# ALLOWED_HOSTS = [
+#     '127.0.0.1'
+# ]
 
 
 # Application definition
