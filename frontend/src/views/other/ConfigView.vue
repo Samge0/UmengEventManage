c994915ed3676544eb0b2854e4222b5aa7528ee1
Samge <948331124@qq.com>
2022-01-04 10:08:50 +0800

    增加获取友盟cookie的步骤提示

--- frontend/src/views/other/ConfigView.vue
+++ frontend/src/views/other/ConfigView.vue
@@ -1,7 +1,8 @@
 <!--配置管理-->
 <template>
-<el-container direction="vertical"
-                  class="demo-shadow">
+<el-container
+    direction="vertical"
+    class="demo-shadow">
 
       <el-header style="height:auto;" >
         <el-row class="box-header" type="flex" justify="space-between">
@@ -11,40 +12,47 @@
           </el-col>
 
           <el-col span="6">
+           <el-button size="mini" type="primary" icon="el-icon-setting" @click="login">友盟官网（去登录获取cookie等）</el-button>
           </el-col>
 
         </el-row>
       </el-header>
 
+    <el-divider ></el-divider>
 
-      <el-divider ></el-divider>
-
+<!--  获取cookie等配置信息引导-->
+  <el-steps :active="3" finish-status="success" simple="true" style="margin-left: 40px;margin-right: 40px">
+    <el-step title="登录友盟官网" @click="login"></el-step>
+    <el-step title="F12控制台复制参数" @click="login"></el-step>
+    <el-step title="保存配置" @click="addOrUpdateKv"></el-step>
+  </el-steps>
 
     <el-form
     :label-position="right"
     label-width="150px"
     :model="form"
-    style="margin: 20px;width: 95%"
+    style="margin: 20px;width: 96.5%"
   >
 
     <el-form-item label="CONTENT_TYPE" required="true">
-      <el-input v-model="form.CONTENT_TYPE" placeholder="CONTENT_TYPE"></el-input>
+      <el-input v-model="form.CONTENT_TYPE" placeholder="【必填】默认值为application/json;charset=UTF-8"></el-input>
     </el-form-item>
     <el-form-item label="USER_AGENT" required="true">
-      <el-input v-model="form.USER_AGENT" placeholder="USER_AGENT"></el-input>
+      <el-input v-model="form.USER_AGENT" placeholder="【必填】请求头中的[user-agent]字段内容"></el-input>
     </el-form-item>
      <el-form-item label="COOKIE" required="true">
-      <el-input v-model="form.COOKIE" placeholder="【必填】友盟那边登录成功后的cookie"></el-input>
+      <el-input v-model="form.COOKIE" placeholder="【必填】友盟那边登录成功后请求头中的[cookie]字段内容"></el-input>
     </el-form-item>
     <el-form-item label="X_XSRF_TOKEN">
-      <el-input v-model="form.X_XSRF_TOKEN" placeholder="【不用手动填】会自动从cookie中读取"></el-input>
+      <el-input v-model="form.X_XSRF_TOKEN" placeholder="【不用手动填】会自动从cookie中的[XSRF-TOKEN]中读取，如果取不到，请检查是否登录后的cookie"></el-input>
     </el-form-item>
     <el-form-item label="X_XSRF_HAITANG">
-      <el-input v-model="form.X_XSRF_HAITANG" placeholder="【不用手动填】会自动从cookie中读取"></el-input>
+      <el-input v-model="form.X_XSRF_HAITANG" placeholder="【不用手动填】会自动从cookie中的[XSRF-TOKEN-HAITANG]中读取，如果取不到，请检查是否登录后的cookie"></el-input>
     </el-form-item>
   </el-form>
 
   <el-button type="primary" size="mini" @click="addOrUpdateKv" icon="el-icon-upload" style="margin-left: 45%;margin-right: 45%">保存</el-button>
+
   </el-container>
 
 </template>
@@ -55,6 +63,7 @@ import {api} from "@/axios/api";
 import {toast} from "@/utils/toast";
 
 export default defineComponent({
+
   components: {
   },
   created() {
@@ -101,10 +110,17 @@ export default defineComponent({
           })
     }
 
+    // 去友盟官网登录
+    const toLoginUm = () => {
+      console.log("点击login")
+      window.open("https://mobile.umeng.com/platform/apps/list", "_blank");
+    }
+
     return {
       ...toRefs(state),
       addOrUpdateKv,
       getConfig,
+      login: toLoginUm,
     }
   },
 })
