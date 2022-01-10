9505ee9753d4d9431794ec7314fa7f81c5224728
Samge <948331124@qq.com>
2022-01-10 11:32:31 +0800

    解决页面四周有空白间距问题

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,7 +12,9 @@ import HomeView from './views/home/Home.vue';
   },
   name: "App"
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+}
 </script>
 
 <style lang="scss">
@@ -21,5 +23,13 @@ export default class App extends Vue {}
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+
+/**
+* 解决页面四周有空白间距问题
+*/
+body{
+ margin:0;
+ padding:0;
 }
 </style>
